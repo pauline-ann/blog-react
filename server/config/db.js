ed58a9994e083aa617386a112f37bc901facccc8
pauline-ann <paulinebantayan@gmail.com>
2019-09-11 17:14:47 -0700

    add db config

---
+++ server/config/db.js
@@ -0,0 +1,26 @@
+// ----- MongoDB Database Config --------------------/
+
+const mongoose = require('mongoose');
+
+// Configure env. variables in dotenv file
+require('dotenv').config();
+const uri = process.env.ATLAS_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            uri,
+            {
+                useNewUrlParser: true,
+                useCreateIndex: true
+            }
+        );
+
+        console.log('MongoDB connection success.');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -4,27 +4,14 @@
 const express = require("express");
 const cors = require('cors');
 const logger = require("morgan");
-const mongoose = require("mongoose");
 const favicon = require('express-favicon');
 const path = require('path');
-const Data = require('./data.js');
+const connectDB = require("./config/db");
 
 const router = express.Router();
 
-// Configure env. variables in dotenv file
-require('dotenv').config();
-
 // ----- MongoDB Database --------------------/
-
-const uri = process.env.ATLAS_URI;
-
-// Connect backend code with database
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-let connection = mongoose.connection;
-
-// Check if connected
-connection.once('open', () => console.log('MongoDB connection established successfully!'));
-connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+connectDB();
 
 // ----- Setup --------------------/
 
@@ -48,56 +35,6 @@ app.use(logger('dev'));
 // Test path for router
 app.get('/ping', function (req, res) {
     return res.send('pong');
-});
-
-// this is our get method
-// this method fetches all available data in our database
-router.get('/getData', (req, res) => {
-    Data.find((err, data) => {
-        if (err) return res.json({ success: false, error: err });
-        return res.json({ success: true, data: data });
-    });
-});
-
-// this is our update method
-// this method overwrites existing data in our database
-router.post('/updateData', (req, res) => {
-    const { id, update } = req.body;
-    Data.findByIdAndUpdate(id, update, (err) => {
-        if (err) return res.json({ success: false, error: err });
-        return res.json({ success: true });
-    });
-});
-
-// this is our delete method
-// this method removes existing data in our database
-router.delete('/deleteData', (req, res) => {
-    const { id } = req.body;
-    Data.findByIdAndRemove(id, (err) => {
-        if (err) return res.send(err);
-        return res.json({ success: true });
-    });
-});
-
-// this is our create methid
-// this method adds new data in our database
-router.post('/putData', (req, res) => {
-    let data = new Data();
-
-    const { id, message } = req.body;
-
-    if ((!id && id !== 0) || !message) {
-        return res.json({
-            success: false,
-            error: 'INVALID INPUTS',
-        });
-    }
-    data.message = message;
-    data.id = id;
-    data.save((err) => {
-        if (err) return res.json({ success: false, error: err });
-        return res.json({ success: true });
-    });
 });
 
 // Define any API routes before this runs

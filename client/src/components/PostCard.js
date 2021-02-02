c045c41bf419729748f5a5cda86ab8dad184786a
pauline-ann <paulinebantayan@gmail.com>
2021-02-02 15:09:53 -0800

    add category to post cards

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -6,16 +6,22 @@ import sample from "../assets/images/ramen.jpg";
 
 const PostCard = (props) => {
 
-    const postCardLink = {
+    const postCardHeader = {
         'font-size': '1.3rem',
         'font-weight': '700 !important'
+    }
+
+    const postCardCategory = {
+        'color': 'rgb(231, 159, 49)',
+        'text-transform': 'capitalize'
     }
 
     return (
         <Item>
             <Item.Image size='medium' src={sample} />
             <Item.Content>
-                <Link to={"/post/" + props.id}><Item.Header as='a' style={postCardLink}>{props.title}</Item.Header></Link>
+                <Item.Meta style={postCardCategory}>{props.category}</Item.Meta>
+                <Link to={"/post/" + props.id}><Item.Header as='a' style={postCardHeader}>{props.title}</Item.Header></Link>
                 <Item.Description>{props.description}</Item.Description>
                 <Item.Extra>
                     Aesthetic: <Rating icon='star' defaultRating={props.aesthetic} maxRating={5} />

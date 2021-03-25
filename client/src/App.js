a1745bf8bdd3df72c1e7906b1d903d728ac28ad6
pauline-ann <paulinebantayan@gmail.com>
2021-03-24 21:19:13 -0700

    sticky footer

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Post from "./components/Post";
 import UpdateModal from "./components/UpdateModal";
 import About from "./components/About/About";
 import Error404 from "./components/Error404";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -19,13 +19,13 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={() => <Home />} />
           <Route exact path="/dashboard" component={() => <Dashboard />} />
-          <Route path="/post/:id" component={(props) => <Post {...props}/>} />
+          <Route path="/post/:id" component={(props) => <Post {...props} />} />
           <Route exact path="/edit/:id" component={() => <UpdateModal />} />
           <Route exact path="/about" component={() => <About />} />
           <Route component={Error404} />
         </Switch>
-        <Footer />
       </div>
+      <Footer />
     </Router>
   )
 }
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,20 +3,17 @@ import React from 'react';
 import { Container, Image, List, Divider } from 'semantic-ui-react'
 
 //Assets
-import logo from '../assets/images/pb_logo_grey.png';
+import logo from '../../assets/images/pb_logo_grey.png';
+
+//CSS
+import './Footer.css'
 
 const Footer = () => {
 
-  const footerStyle = {
-    marginTop: '3em',
-    paddingTop: '1em',
-    paddingBottom: '1em',
-  }
-
   return (
-    <div style={footerStyle}>
+    <div className='footer'>
       <Container textAlign='center'>
-        <Divider section hidden/>
+        <Divider section hidden />
         <a href='https://www.paulineann.me/' target='_blank' rel='noopener noreferrer'><Image src={logo} size='mini' centered /></a><br />
         <List horizontal>
           <List.Item>Designed and Developed by <a href='https://www.linkedin.com/in/pauline-ann/' target='_blank' rel='noopener noreferrer'>Pauline Ann Bantayan</a> — <a href='https://reactjs.org/'>Powered by React.js</a> — © 2019-2021</List.Item>

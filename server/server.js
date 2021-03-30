0bd93680da4d497105e2e6a2d1166efdcb7024eb
pauline-ann <paulinebantayan@gmail.com>
2021-03-30 14:55:13 -0700

    Version 1.0.0

--- server/server.js
+++ server/server.js
@@ -5,27 +5,14 @@ const express = require("express");
 const cors = require('cors');
 const logger = require("morgan");
 const favicon = require('express-favicon');
+const path = require('path');
 
 // ----- Init Mongo DB --------------------/
 const connectDB = require("./config/db");
 connectDB();
 
-// ----- Setup --------------------/
-
-// Create express server
-const port = process.env.PORT || 8080;
+// ----- Init Express Server --------------------/
 const app = express();
-
-// Serve up static assets
-app.use(favicon(__dirname + '../client/public/favicon.ico'));
-app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, '../build')));
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(logger('dev'));
 
 // ----- Mount Router --------------------/
 
@@ -34,11 +21,6 @@ app.get('/ping', function (req, res) {
     return res.send('<p>pong</p>');
 });
 
-//Define any API routes before this runs
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, '../build', 'index.html'));
-// });
-
 // Append /api routing for our http requests
 const postsRouter = require('./routes/api/posts');
 app.use('/api/posts', postsRouter);
@@ -46,7 +28,30 @@ app.use('/api/posts', postsRouter);
 const imagesRouter = require('./routes/api/images');
 app.use('/api/images', imagesRouter);
 
+// Serve up static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('../client/build'));
+    app.use(favicon(__dirname + '../client/build/favicon.ico'));
+
+    // Define any API routes before this runs
+    // Any requests that's not to the API...
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    })
+} else {
+    app.use(favicon(__dirname + '../client/public/favicon.ico'));
+}
+
+// ----- Middleware --------------------/
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+
 // ----- Listen --------------------/
+const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
     console.log("------------------------------------------------------------");
     console.log(`React Blog application running on port ${port}...`);
